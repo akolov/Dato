76dd7162579406f6d0899fab4c219101f7d4b969
Alexander Kolov <me@alexkolov.com>
2014-05-12 22:53:38 +0200

    Update project settings

---
+++ bump-version.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ ! -z "$(git status --porcelain)" ]; then
+  echo "Please commit all changes before bumping version"
+  exit 1
+fi
+
+xcrun agvtool bump -all &> /dev/null
+
+MARKETING_VERSION=$(xcrun agvtool what-marketing-version -terse1 | egrep '^\d+')
+BUILD_VERSION=$(xcrun agvtool what-version -terse)
+FULL_VERSION="${MARKETING_VERSION}-${BUILD_VERSION}"
+
+git tag -a -m ${FULL_VERSION} ${FULL_VERSION}
+git commit -a -m "Bump version to ${FULL_VERSION}"
+git push --all
+
+echo "Successfully bumped version to ${FULL_VERSION}"
